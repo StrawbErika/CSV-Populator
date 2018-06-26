6cb112ef401e3ab418edc8a453924dc313c89536
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2018-06-26 12:53:35 +0800

    + gets first 200 of a category and saves to bookList.txt

--- converter.py
+++ converter.py
@@ -1,4 +1,45 @@
+def save_file(data, name):
+    file = open(name + ".txt","w") 
+    num = 0
+    while(num != len(data)):
+        file.write(str(data[num])+"\n")
+        num = num + 1        
+    file.close() 
+
 file = open("book32listing.csv", "r") 
 book = file.readlines() 
-print(type(book))
-print(len(book))
+fileList = []
+
+for x in range(0, len(book)):
+    lineList = book[x].split(",")
+    fileList.append(lineList)
+
+categoryList = []
+officialList = []
+x = 0
+
+count = 0
+while(x<len(fileList)):
+    category = fileList[x][-1]
+    try:
+        number=categoryList.index(category)
+    except ValueError:
+        count = count + 1
+        inList = 'false'
+    else:
+        inList = 'true'
+    if((categoryList == 0) or (inList == 'false')):
+        categoryList.append(category)
+        y = 0 
+        for y in range(x, x+201):
+            if(fileList[y][-1] != category):
+                break;
+            else:
+                officialList.append(fileList[y])
+        category = fileList[y][-1]
+        x = x + 200
+    x = x + 1    
+
+def getFirst200OfCategory():
+"Amazon Index","Filename","Image","Title","Author","Category ID","Category"
+0                 1         2        3      4          5
