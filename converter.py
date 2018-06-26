9553cfef3906e7ebb33b2350112a3b68b581045d
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2018-06-26 13:03:33 +0800

    + deleteUnnecessaryData, deletes fileName, Image * getCategory by index -1

--- converter.py
+++ converter.py
@@ -1,3 +1,7 @@
+def deleteUnnecessaryData(data):
+    for num in range(0, len(data)):
+        del data[num][1:3]
+
 def save_file(data, name):
     file = open(name + ".txt","w") 
     num = 0
@@ -6,40 +10,47 @@ def save_file(data, name):
         num = num + 1        
     file.close() 
 
-file = open("book32listing.csv", "r") 
-book = file.readlines() 
-fileList = []
-
-for x in range(0, len(book)):
-    lineList = book[x].split(",")
-    fileList.append(lineList)
-
-categoryList = []
-officialList = []
-x = 0
-
-count = 0
-while(x<len(fileList)):
-    category = fileList[x][-1]
-    try:
-        number=categoryList.index(category)
-    except ValueError:
-        count = count + 1
-        inList = 'false'
-    else:
-        inList = 'true'
-    if((categoryList == 0) or (inList == 'false')):
-        categoryList.append(category)
-        y = 0 
-        for y in range(x, x+201):
-            if(fileList[y][-1] != category):
-                break;
-            else:
-                officialList.append(fileList[y])
-        category = fileList[y][-1]
-        x = x + 200
-    x = x + 1    
-
 def getFirst200OfCategory():
-"Amazon Index","Filename","Image","Title","Author","Category ID","Category"
-0                 1         2        3      4          5
+    file = open("book32listing.csv", "r") 
+    book = file.readlines() 
+    fileList = []
+
+    for x in range(0, len(book)):
+        lineList = book[x].split(",")
+        fileList.append(lineList)
+
+    categoryList = []
+    officialList = []
+    x = 0
+
+    count = 0
+    while(x<len(fileList)):
+        category = fileList[x][-1]
+        try:
+            number=categoryList.index(category)
+        except ValueError:
+            count = count + 1
+            inList = 'false'
+        else:
+            inList = 'true'
+        if((categoryList == 0) or (inList == 'false')):
+            categoryList.append(category)
+            y = 0 
+            for y in range(x, x+201):
+                if(fileList[y][-1] != category):
+                    break;
+                else:
+                    officialList.append(fileList[y])
+            category = fileList[y][-1]
+            x = x + 200
+        x = x + 1    
+
+    return officialList
+bookList = getFirst200OfCategory()
+deleteUnnecessaryData(bookList)
+save_file(bookList, "officialBookList")
+
+    
+
+# "Amazon Index","Filename","Image","Title","Author","Category ID","Category"
+# 0                 1         2        3      4          5
